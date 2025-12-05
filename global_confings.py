3a62ecc99c2cc89d5fe05ae392a1142812df2b15
GICA <yuhaoqiang62@gmail.com>
2025-12-05 14:52:26 +0800

    Update global_confings.py

--- global_confings.py
+++ global_confings.py
@@ -23,4 +23,24 @@ def set_dataset_config(dataset_name):
         TEXT_DIM = config["TEXT_DIM"]
     else:
         raise ValueError(f"Invalid dataset name: {dataset_name}")
+import os
+import torch
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+os.environ["WANDB_PROGRAM"] = "multimodal_driver.py"
+
+DEVICE = torch.device("cuda:0")
+
+# MOSI SETTING
+ACOUSTIC_DIM = 74
+VISUAL_DIM = 47
+TEXT_DIM = 768
+
+# MOSEI SETTING
+#ACOUSTIC_DIM = 74
+#VISUAL_DIM = 35
+#TEXT_DIM = 768
+
+XLNET_INJECTION_INDEX = 1
+
 
